8566b8090717e3c14427016d0d346d6143575891
allenhung1025 <hungtungming@gmail.com>
2021-11-01 14:50:37 +0800

    p1 scripts

---
+++ hw1_1.sh
@@ -0,0 +1,4 @@
+python3 p1/inference.py --input_dir $1 \
+                        --output_csv $2 \
+                        --model_path p1/checkpoint_vggbn_224_deepest_feature_linear_scheduler_finetune/model_10.pth \
+                        --model_name Vgg19_bn_linear
